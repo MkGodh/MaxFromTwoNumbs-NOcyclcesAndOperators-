498c7d06995dfd87c34f034af75c473c7f397a85
MkGodh <fenryrvla@yandex.ru>
2024-04-05 19:04:38 +0300

    Добавьте файлы проекта.

---
+++ MaxFromTwoNumbs(NOcyclcesAndOperators)/MaxFromTwoNumbs(NOcyclcesAndOperators).cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <cstring>
+#include <cmath>
+#include <iomanip>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+int main() {
+	float a;
+	float b;
+	cin >> a >> b;
+
+	float middle = (a + b) / 2;
+	float diff = fabs(a - b);
+
+	float max = middle + diff / 2;
+	float min = middle - diff / 2;
+
+	cout << max << "\n" << min;
+}
